e092f5e83556899d249e96fa49589aee7c2b4260
vishwageethanjana <geeth.me67@gmail.com>
2026-02-06 06:27:32 +0530

    requirements.txt

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql import func
 from backend.app.database import Base
 
+
 class User(Base):
     __tablename__ = "users"
 
