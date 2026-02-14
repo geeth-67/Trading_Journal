149621dbaf502971d51eddf98cf0884346f62262
vishwageethanjana <geeth.me67@gmail.com>
2026-02-14 11:18:50 +0530

    modify main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,29 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware  # <--- Import this
+from fastapi.middleware.cors import CORSMiddleware
 from backend.app.database import Base, engine
 from backend.app.routes import auth
 
+from backend.app.routes import auth, trade
+
 Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
-# --- ADD THIS BLOCK ---
-# This is the "VIP List". We allow everyone ("*") for now to make it easy.
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods (POST, GET, etc.)
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-# ----------------------
+
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+app.include_router(trade.router, prefix="/trades", tags=["Trades"])
+
 
 @app.get("/")
 def home():
-    return {"message": "Welcome to Trading Journal API"}
+    return {"message": "Welcome to Trading Journal"}
